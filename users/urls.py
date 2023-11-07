957b0de9f87d3feb807e982d4bf9ebf875c94ba8
ShaunV <2102172@sit.singaporetech.edu.sg>
2023-11-07 19:20:41 +0800

    search version

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import home, profile, RegisterView, search_view, search_results
+from .views import home, search_view, search_results
 
 urlpatterns = [
-    path('', home, name='users-home'),
-    path('register/', RegisterView.as_view(), name='users-register'),
-    path('profile/', profile, name='users-profile'),
+    path('', search_view, name='users-home'),
     path('search/', search_view, name='search-view'),
     path('results/', search_results, name='search-results'),
 ]
